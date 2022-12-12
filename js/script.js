b8247c6e0b44d9d47782436fd2553eb2709dd72c
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2022-12-12 21:53:14 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+{
+    const tasks = [
+        {
+            content: "zrobić zakupy",
+            done: false,
+
+        },
+        {
+            content: "zjeść obiad",
+            done: true,
+        },
+
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                    ${task.content}
+                 </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+
+}
