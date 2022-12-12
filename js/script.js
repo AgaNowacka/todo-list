a872c056f559ba5cf4bda8643b5f370c544e84b8
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2022-12-12 22:53:12 +0100

    removing tasks

--- js/script.js
+++ js/script.js
@@ -12,20 +12,6 @@
 
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-                    ${task.content}
-                 </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -34,7 +20,35 @@
         render();
     };
 
-    const onFormSubmit = (event) => {
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                    <button class="js-removeButton">Usuń</button>
+                    ${task.content}
+                 </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-removeButton");
+        
+        removeButtons.forEach ((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+        const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
