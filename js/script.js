465df76c39278e086952e322274ebac5749ac22c
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2022-12-13 21:52:08 +0100

    toggle task done

--- js/script.js
+++ js/script.js
@@ -25,18 +25,25 @@
         render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
                 <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                    <button class="js-doneButton">zrobione?</button>
                     <button class="js-removeButton">Usuń</button>
                     ${task.content}
                  </li>
             `;
         }
 
+        
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
         const removeButtons = document.querySelectorAll(".js-removeButton");
@@ -44,6 +51,14 @@
         removeButtons.forEach ((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-doneButton");
+        
+        toggleDoneButtons.forEach ((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
